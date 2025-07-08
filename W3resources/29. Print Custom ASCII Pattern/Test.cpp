ed45c6f26cf4dd7547491abfc305fd637a348caa
BiiiHaaa <chaoufi.brahim@univ-bechar.dz>
2025-07-08 10:02:13 +0100

    I made it

--- W3resources/29. Print Custom ASCII Pattern/Test.cpp
+++ W3resources/29. Print Custom ASCII Pattern/Test.cpp
@@ -20,5 +20,23 @@ x     x       x        x
 using namespace std;
 
 int main() {
-    
+    for(int i =0;i<27;i++) {
+        if(i%2==0) {
+            cout << endl;
+        }else {
+            for(int j=0;j<7;j++) {
+                if(j==0 || j==6) {
+                    cout << " xxxxx                     "<<endl;
+                }else if(j==1 || j==5) {
+                    cout << "x     x       x        x  "<<endl;
+                }else if(j==2 ||  j==4) {
+                    cout << "x             x        x  "<<endl;
+                }
+                else {
+                    cout << "x          xxxxxxx  xxxxxxx"<<endl;
+                }
+            }
+            break;
+        }
+    }
 }
